36d53f38c6f1322f3d20b5e284fc512c2b9080b9
Barghout22 <barghoutmahmoud2@gmail.com>
2025-09-04 13:51:14 +0300

    Added the basic classes for the project Created the context file and the DB

---
+++ Models/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    internal enum Status { Pending,InProgress,Completed}
+    internal class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; } // Low, Medium, High
+        public Status Status { get; set; } // Pending, In Progress, Completed
+
+    }
+}
---
+++ Models/TaskManagerContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    internal class TaskManagerContext:DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<TaskItem> TaskItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer("Server=MAHMOUD\\SQLEXPRESS;Database=TaskManager;Trusted_Connection=True;Encrypt=False");
+        }
+    }
+}
