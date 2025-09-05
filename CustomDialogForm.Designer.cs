768a8ef28486cd4dd6542c98767873543adcb468
Barghout22 <barghoutmahmoud2@gmail.com>
2025-09-05 15:10:23 +0300

    Added the basic layout for task addition form. Added the buttons and layout items for the task addition form. Added a condition to check if there is a selected user and get tasks based on them. added an option to add users or switch between users.

--- CustomDialogForm.Designer.cs
+++ CustomDialogForm.Designer.cs
@@ -28,12 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CustomDialogForm";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(73, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Task title";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(73, 124);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Task Description";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(317, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Due date";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(319, 98);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Priorty";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(319, 160);
+            label5.Name = "label5";
+            label5.Size = new Size(39, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Status";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(319, 228);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Category";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(133, 335);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(317, 335);
+            button2.Name = "button2";
+            button2.RightToLeft = RightToLeft.No;
+            button2.Size = new Size(102, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(73, 54);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(211, 23);
+            textBox1.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(73, 142);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(211, 146);
+            richTextBox1.TabIndex = 9;
+            richTextBox1.Text = "";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(319, 54);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(319, 116);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(200, 23);
+            comboBox1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(319, 178);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(200, 23);
+            comboBox2.TabIndex = 12;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(319, 258);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(200, 23);
+            comboBox3.TabIndex = 13;
+            // 
+            // CustomDialogForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(581, 410);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CustomDialogForm";
+            Text = "CustomDialogForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private DateTimePicker dateTimePicker1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
     }
 }
